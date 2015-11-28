28747f30a977adeff61359b98aa23d54fc5a536a
[01;31m[K"name"[m[K: "Christopher \"Aji\" Slater <[01;31m[K"email"[m[K: null>
2015-11-28 15:45:15 -0600

    switches to erb and partials methodology

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,3 +1,7 @@
+require_relative "../helpers/hashes"
+
 get '/' do
+  @hash = Hashy_hash
+  p @hash
   erb :index
 end
---
+++ app/helpers/hashes.rb
@@ -0,0 +1,101 @@
+Hashy_hash = {
+
+  "::QueryMethods" => {
+
+    :color     =>  "bg-one",
+    :href      =>  "",
+    :list      =>
+
+  [{ :name => ".count_by_sql(sql)",
+    :href => "http://api.rubyonrails.org/classes/ActiveRecord/Querying.html#method-i-count_by_sql" },
+  { :name => ".create_with(value)",
+    :href => "http://api.rubyonrails.org/classes/ActiveRecord/QueryMethods.html#method-i-create_with" },
+  { :name => ".distinct",
+    :href => "http://api.rubyonrails.org/classes/ActiveRecord/QueryMethods.html#method-i-distinct" },
+  { :name => ".eager_load",
+    :href => "http://api.rubyonrails.org/classes/ActiveRecord/QueryMethods.html#method-i-eager_load" },
+  { :name => ".extending",
+    :href => "http://api.rubyonrails.org/classes/ActiveRecord/QueryMethods.html#method-i-extending" },
+  { :name => ".find_by_sql(sql)",
+    :href => "http://api.rubyonrails.org/classes/ActiveRecord/Querying.html#method-i-find_by_sql" },
+  { :name => ".from",
+    :href => "http://api.rubyonrails.org/classes/ActiveRecord/QueryMethods.html#method-i-from" },
+  { :name => ".group(*args)",
+    :href => "http://api.rubyonrails.org/classes/ActiveRecord/QueryMethods.html#method-i-group" },
+  { :name => ".having(opts)",
+    :href => "http://api.rubyonrails.org/classes/ActiveRecord/QueryMethods.html#method-i-having" },
+  { :name => ".includes(*args)",
+    :href => "http://api.rubyonrails.org/classes/ActiveRecord/QueryMethods.html#method-i-includes" },
+  { :name => ".joins(*args)",
+    :href => "http://api.rubyonrails.org/classes/ActiveRecord/QueryMethods.html#method-i-joins" },
+  { :name => ".limit(value)",
+    :href => "http://api.rubyonrails.org/classes/ActiveRecord/QueryMethods.html#method-i-limit" },
+  { :name => ".lock",
+    :href => "http://api.rubyonrails.org/classes/ActiveRecord/QueryMethods.html#method-i-lock" },
+  { :name => ".none",
+    :href => "http://api.rubyonrails.org/classes/ActiveRecord/QueryMethods.html#method-i-none" },
+  { :name => ".offset(value)",
+    :href => "http://api.rubyonrails.org/classes/ActiveRecord/QueryMethods.html#method-i-offset" },
+  { :name => ".order(*args)",
+    :href => "http://api.rubyonrails.org/classes/ActiveRecord/QueryMethods.html#method-i-order" },
+  { :name => ".preload(*args)",
+    :href => "http://api.rubyonrails.org/classes/ActiveRecord/QueryMethods.html#method-i-preload" },
+  { :name => ".readonly",
+    :href => "http://api.rubyonrails.org/classes/ActiveRecord/QueryMethods.html#method-i-readonly" },
+  { :name => ".references(table_name)",
+    :href => "http://api.rubyonrails.org/classes/ActiveRecord/QueryMethods.html#method-i-references" },
+  { :name => ".reorder(*args)",
+    :href => "http://api.rubyonrails.org/classes/ActiveRecord/QueryMethods.html#method-i-reorder" },
+  { :name => ".reverse_order",
+    :href => "http://api.rubyonrails.org/classes/ActiveRecord/QueryMethods.html#method-i-reverse_order" },
+  { :name => ".rewhere(conditions)",
+    :href => "http://api.rubyonrails.org/classes/ActiveRecord/QueryMethods.html#method-i-rewhere" },
+  { :name => ".select(*fields)",
+    :href => "http://api.rubyonrails.org/classes/ActiveRecord/QueryMethods.html#method-i-select" },
+  { :name => ".uniq",
+    :href => "http://api.rubyonrails.org/classes/ActiveRecord/QueryMethods.html#method-i-uniq" },
+  { :name => ".unscope",
+    :href => "http://api.rubyonrails.org/classes/ActiveRecord/QueryMethods.html#method-i-unscope" },
+  { :name => ".where",
+    :href => "http://api.rubyonrails.org/classes/ActiveRecord/QueryMethods.html#method-i-where" }]
+  },
+
+  "::Persistence" => {
+    :color     =>  "bg-two",
+    :href      =>  ""
+    list       => [{
+
+      { :name => ".becomes(klass)",
+        :href => "http://api.rubyonrails.org/classes/ActiveRecord/Persistence.html#method-i-becomes" },
+      { :name => ".becomes!(kiss)",
+        :href => "http://api.rubyonrails.org/classes/ActiveRecord/Persistence.html#method-i-becomes-21" },
+      { :name => ".decrement(attribute, by = 1)",
+        :href => "http://api.rubyonrails.org/classes/ActiveRecord/Persistence.html#method-i-decrement" },
+      { :name => ".decrement!(attribute, by = 1",
+        :href => "http://api.rubyonrails.org/classes/ActiveRecord/Persistence.html#method-i-decrement-21" },
+      { :name => ".delete",
+        :href => "http://api.rubyonrails.org/classes/ActiveRecord/Persistence.html#method-i-delete" },
+      { :name => ".destroy",
+        :href => "http://api.rubyonrails.org/classes/ActiveRecord/Persistence.html#method-i-destroy" },
+      { :name => ".destroy!",
+        :href => "http://api.rubyonrails.org/classes/ActiveRecord/Persistence.html#method-i-destroy" },
+      { :name => ".destroyed?",
+        :href => "http://api.rubyonrails.org/classes/ActiveRecord/Persistence.html#method-i-destroyed-3F" },
+      { :name => ".incrememnt(attribute, by = 1"),
+        :href => "http://api.rubyonrails.org/classes/ActiveRecord/Persistence.html#method-i-increment" },
+
+      { :name => ".increment!(attribute, by = 1)",
+        :href => "" },
+    }]
+  },
+
+
+
+
+
+
+
+
+
+
+}
